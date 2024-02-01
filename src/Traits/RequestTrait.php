da6b7ae66566352fed88171ee1f39306d304a742
MCKLtech <MCKLtech@gmail.com>
2024-02-01 10:00:09 -0500

    Update RequestTrait.php
    - Added removal helper for null fields in User Custom Profile Fields.

--- src/Traits/RequestTrait.php
+++ src/Traits/RequestTrait.php
@@ -12,6 +12,16 @@ trait RequestTrait
      */
     public function removeEmptyArrayValues(array $array): array
     {
+
+        /**
+         * Remove empty values from custom_profile_fields as it can cause requests to fail
+         */
+        if (isset($array['custom_profile_fields']) && is_array($array['custom_profile_fields'])) {
+            $array['custom_profile_fields'] = array_filter($array['custom_profile_fields'], function ($field) {
+                return !empty($field['value']) && !empty($field['id']);
+            });
+        }
+
         return array_filter($array, fn($value) => $value !== null);
     }
 }
