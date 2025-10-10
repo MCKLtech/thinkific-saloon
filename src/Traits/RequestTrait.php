1c47012e0f09d9d22253172dd6e373085dd9e3d4
MCKLtech <MCKLtech@gmail.com>
2025-10-10 11:04:12 -0400

    Update RequestTrait.php
    - Filter out fields from custom_profile_fields when creating/updating

--- src/Traits/RequestTrait.php
+++ src/Traits/RequestTrait.php
@@ -12,14 +12,21 @@ trait RequestTrait
      */
     public function removeEmptyArrayValues(array $array): array
     {
-
         /**
          * Remove empty values from custom_profile_fields as it can cause requests to fail
          */
         if (isset($array['custom_profile_fields']) && is_array($array['custom_profile_fields'])) {
             $array['custom_profile_fields'] = array_filter($array['custom_profile_fields'], function ($field) {
-                return !empty($field['value']) && !empty($field['id']);
+                return !empty($field['value']) && !empty($field['custom_profile_field_definition_id']);
             });
+
+            // Update/Create should only contain "value" and "custom_profile_field_definition_id"
+            $array['custom_profile_fields'] = array_map(function ($field) {
+                return [
+                    'value' => $field['value'] ?? '',
+                    'custom_profile_field_definition_id' => $field['custom_profile_field_definition_id'] ?? '',
+                ];
+            }, $array['custom_profile_fields']);
         }
 
         return array_filter($array, fn($value) => $value !== null);
