cef049098c837bd9510ffb79776c5e53b79ef57e
MCKLtech <MCKLtech@gmail.com>
2025-12-16 10:34:47 -0500

    Fixed Authenticator
    - Added Order Handling for Unknown Products

--- src/Auth/ThinkificAuthenticator.php
+++ src/Auth/ThinkificAuthenticator.php
@@ -4,6 +4,7 @@ namespace WooNinja\ThinkificSaloon\Auth;
 
 use Saloon\Contracts\Authenticator;
 use Saloon\Http\PendingRequest;
+
 class ThinkificAuthenticator implements Authenticator
 {
 
@@ -18,7 +19,11 @@ class ThinkificAuthenticator implements Authenticator
     public
     function set(PendingRequest $pendingRequest): void
     {
-        if ($this->is_oauth) {
+        /**
+         * OAuth and "API Access Tokens" are Bearer, not custom headers
+         * REST API Keys are 32 characters long by default
+         */
+        if ($this->is_oauth || strlen($this->api_key) > 32) {
             $pendingRequest->headers()->add('Authorization', 'Bearer ' . $this->api_key);
         } else {
             $pendingRequest->headers()->add('X-Auth-API-Key', $this->api_key);
