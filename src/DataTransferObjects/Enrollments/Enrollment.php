99c3ec8534ecdb656c2e051aab86877dd5688a3b
MCKLtech <MCKLtech@gmail.com>
2026-02-19 12:17:05 -0500

    Update Enrollment.php

--- src/DataTransferObjects/Enrollments/Enrollment.php
+++ src/DataTransferObjects/Enrollments/Enrollment.php
@@ -8,40 +8,40 @@ use WooNinja\LMSContracts\Contracts\DTOs\Enrollments\EnrollmentInterface;
 final class Enrollment implements EnrollmentInterface
 {
     public function __construct(
-        public int     $id,
-        public string  $user_email,
-        public string  $user_name,
-        public int     $user_id,
-        public string  $course_name,
-        public int     $course_id,
-        public float   $percentage_completed,
-        public bool    $expired,
-        public bool    $is_free_trial,
-        public bool    $completed,
-        public ?Carbon $started_at,
-        public ?Carbon $activated_at,
-        public ?Carbon $completed_at,
-        public Carbon  $updated_at,
-        public ?Carbon $expiry_date,
+        public int|string $id,
+        public string     $user_email,
+        public string     $user_name,
+        public int        $user_id,
+        public string     $course_name,
+        public int        $course_id,
+        public float      $percentage_completed,
+        public bool       $expired,
+        public bool       $is_free_trial,
+        public bool       $completed,
+        public ?Carbon    $started_at,
+        public ?Carbon    $activated_at,
+        public ?Carbon    $completed_at,
+        public Carbon     $updated_at,
+        public ?Carbon    $expiry_date,
 
         /**
          * @var string|null
          * Only available from the enrollment.completed webhook at present (Dec/2024)
          */
-        public ?string $credential_id,
+        public ?string    $credential_id,
 
         /**
          * @var string|null
          * Only available from the enrollment.completed webhook at present (Dec/2024)
          */
-        public ?string $certificate_url,
+        public ?string    $certificate_url,
 
         /**
          * @var Carbon|null
          * Only available from the enrollment.completed webhook at present (Dec/2024)
          * Unlike other dates in Thinkific, this date format is Y-m-d
          */
-        public ?Carbon $certificate_expiry_date,
+        public ?Carbon    $certificate_expiry_date,
     )
     {
     }
